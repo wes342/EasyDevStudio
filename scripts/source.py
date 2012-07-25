a844a8d607f8fae5cb29a2e261343c04cc95f3b0
wes <wes@wes-envy.(none)>
2012-07-25 00:41:36 -0700

    Added and Rearanged Popups for source building Partially added sliders for sync and make jobs @lithid: I dont know how you want to pull supported devices list.          I Added a bunch just so you could see the layout.

--- scripts/source.py
+++ scripts/source.py
@@ -13,14 +13,18 @@
 #limitations under the License.
 
 #!/usr/bin/env python
+
+
 from scripts.GI import *
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.switch import Switch
 from kivy.uix.settings import SettingItem, SettingsPanel, SettingOptions
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.slider import Slider
 import commands
 import platform
+
 
 def no_os(self):
     root = BoxLayout(orientation='vertical', spacing=20)
@@ -29,7 +33,7 @@ def no_os(self):
     root.add_widget(Label(halign="center", text='You Must Be Running Linux to Build from source.\nCurrent Supported Distros are:\n"Ubuntu" Or "Linux Mint"'))
     root.add_widget(btn_layout)
     btn_layout.add_widget(cancel)
-    popup = Popup(background='atlas://images/eds/pop', title='Add Option',content=root, auto_dismiss=False,
+    popup = Popup(background='atlas://images/eds/pop', title='Unsupported OS',content=root, auto_dismiss=False,
     size_hint=(None, None), size=(350, 200))
     cancel.bind(on_release=popup.dismiss)
     popup.open()
@@ -153,7 +157,7 @@ def install_packages(instance):
         Box = BoxLayout(orientation="vertical", spacing=10)
         msg = GridLayout(cols=1, spacing=0, size_hint_y=None)
         btn_layout = GridLayout(cols=1)
-        btn = CustomButton(text="Done")
+        btn = Button(text="Done")
         btn_layout.add_widget(btn)
         msg.bind(minimum_height=msg.setter('height'))
         for x in p:
@@ -178,7 +182,7 @@ def install_packages(instance):
 def kernel_base(self):
     layout = GridLayout(cols=1, size_hint=(None, 2.5), width=700)
     layout.bind(minimum_height=layout.setter('height'))
-    panel = SettingsPanel(title="Camera Mods", settings=self)   
+    panel = SettingsPanel(title="Kernel Base", settings=self)   
     main = BoxLayout(orientation = 'vertical')
     root = ScrollView(size_hint=(None, None),bar_margin=-11, bar_color=(47 / 255., 167 / 255., 212 / 255., 1.), do_scroll_x=False)
     root.size = (600, 400)
@@ -243,15 +247,15 @@ def kernel_base(self):
     layout.add_widget(i4g)
     
     popup = Popup(background='atlas://images/eds/pop', title='Kernel Base', content=main, auto_dismiss=True, size_hint=(None, None), size=(630, 500))
+    done.bind(on_release=popup.dismiss)
     popup.open()
-
 
 
 def kernel_mods(self):
     Box = BoxLayout(orientation="vertical")
     layout = SettingsPanel(title="Select Mods You Want Added to Kernel", settings=self, size_hint=(1.1, None))
     btn_layout = GridLayout(cols=1)
-    btn = CustomButton(text="Continue")
+    btn = Button(text="Continue")
     btn_layout.add_widget(btn)
     layout.bind(minimum_height=layout.setter('height'))
 
@@ -290,8 +294,6 @@ def pull_conf(self):
 
 def kernel_other(self):
     root = BoxLayout(orentation = 'vertical')
-    done = CustomButton(text='Test')
-    root.add_widget(done)
     scroll = ScrollView(size_hint=(None, 2.5), do_scroll_x=False)
     root.add_widget(scroll)
     btn_layout = GridLayout(cols=1)
@@ -309,16 +311,19 @@ def kernel_other(self):
     done = Button(text ='Done')
     main.add_widget(done)
  
-   
     popup = Popup(background='atlas://images/eds/pop', title='Advanced Kernel Options', content=main, auto_dismiss=True, size_hint=(None, None), size=(630, 500))
+    done.bind(on_release=popup.dismiss)
     popup.open()
-    
+
+
 def aosp_select(self):
     Box = BoxLayout(orientation="vertical", spacing=10)
-    base = SettingsPanel(title="Select Android Base To Start Building", settings=self)
-    btn_layout = GridLayout(cols=1)
-    btn = CustomButton(text="Continue")
-    btn_layout.add_widget(btn)
+    base = SettingsPanel(title="Select AOSP Branch You Would Like To Build From", settings=self)
+    btn_layout = GridLayout(cols=2, spacing=10)
+    select = Button(text="Select")
+    btn_layout.add_widget(select)
+    cancel = Button(text='Cancel')
+    btn_layout.add_widget(cancel)
     base.bind(minimum_height=base.setter('height'))
     
     GB = SettingItem(panel = base, title = "Gingerbread",disabled=False, desc = "Android 2.3,  kernel 2.6.35,  Api 9-10 ")
@@ -345,15 +350,17 @@ def aosp_select(self):
 
     popup = Popup(background='atlas://images/eds/pop', title='AOSP',content=Box, auto_dismiss=True,
     size_hint=(None, None), size=(550, 350))
-    btn.bind(on_release=popup.dismiss)
+    cancel.bind(on_release=popup.dismiss)
     popup.open()
     
 def cm_select(self):
     Box = BoxLayout(orientation="vertical", spacing=10)
-    base = SettingsPanel(title="Select Cyanogen Base to Start Building", settings=self)
-    btn_layout = GridLayout(cols=1)
-    btn = CustomButton(text="Continue")
-    btn_layout.add_widget(btn)
+    base = SettingsPanel(title="Select CyanogenMod Branch You Would Like To Build From.", settings=self)
+    btn_layout = GridLayout(cols=2, spacing=10)
+    select = Button(text="Select")
+    btn_layout.add_widget(select)
+    cancel = Button(text='Cancel')
+    btn_layout.add_widget(cancel)
     base.bind(minimum_height=base.setter('height'))
     
     Cm7 = SettingItem(panel = base, title = "Cyanogen 7",disabled=False, desc = "Android 2.3,  kernel 2.6.35,  Api 9-10 ")
@@ -380,9 +387,250 @@ def cm_select(self):
 
     popup = Popup(background='atlas://images/eds/pop', title='CyanogenMod',content=Box, auto_dismiss=True,
     size_hint=(None, None), size=(550, 350))
-    btn.bind(on_release=popup.dismiss)
+    cancel.bind(on_release=popup.dismiss)
     popup.open()
-   
+
+def branch_select(self):
+    Box = BoxLayout(orientation="vertical", spacing=10)
+    msg = GridLayout(cols=1, padding=15, spacing=10, size_hint_y=None)
+    btn_layout = GridLayout(cols=1)
+    cancel = Button(text="Done")
+    btn_layout.add_widget(cancel)
+    msg.bind(minimum_height=msg.setter('height'))
+
+    aosp = (CustomButton(text='AOSP', font_size=10, size_hint_y=None, height=40))
+    msg.add_widget(aosp)
+    
+    cm = (CustomButton(text='CyanogenMod', font_size=10, size_hint_y=None, height=40))
+    msg.add_widget(cm)
+
+    root = ScrollView(size_hint=(None, None), size=(525, 140), do_scroll_x=False)
+    root.add_widget(msg)
+    Box.add_widget(root)
+    Box.add_widget(btn_layout)
+
+    popup = Popup(background='atlas://images/eds/pop', title='Select Branch',content=Box, auto_dismiss=True,
+    size_hint=(None, None), size=(550, 250))
+    cancel.bind(on_release=popup.dismiss)
+    aosp.bind(on_release=aosp_select)
+    cm.bind(on_release=cm_select)
+    popup.open()
+    
+def device_select(self):
+    layout = GridLayout(cols=1, size_hint=(None, 5.5), width=700)
+    layout.bind(minimum_height=layout.setter('height'))
+    panel = SettingsPanel(title="Select Device", settings=self)   
+    main = BoxLayout(orientation = 'vertical')
+    root = ScrollView(size_hint=(None, None),bar_margin=-11, bar_color=(47 / 255., 167 / 255., 212 / 255., 1.), do_scroll_x=False)
+    root.size = (600, 400)
+    root.add_widget(layout)
+    main.add_widget(root)
+    btn_layout = GridLayout(cols=2, spacing=10)
+    main.add_widget(btn_layout)
+    done = Button(text ='Select')
+    btn_layout.add_widget(done)
+    cancel = Button(text='Cancel')
+    btn_layout.add_widget(cancel)
+
+    aria = SettingItem(panel = panel, title = "Anzu",disabled=False, desc = "")
+    aria_radio = CheckBox(group='device',active=False)
+    aria.add_widget(aria_radio)
+    layout.add_widget(aria)
+
+    inc = SettingItem(panel = panel, title = "Captivatemtd",disabled=False, desc = "")
+    inc_radio = CheckBox(group='device',active=False)
+    inc.add_widget(inc_radio)
+    layout.add_widget(inc)
+    
+    inc2 = SettingItem(panel = panel, title = "Coconut",disabled=False, desc = "")
+    inc2_radio = CheckBox(group='device',active=False)
+    inc2.add_widget(inc2_radio)
+    layout.add_widget(inc2)
+    
+    incs = SettingItem(panel = panel, title = "Crespo",disabled=False, desc = "")
+    incs_radio = CheckBox(group='device',active=False)
+    incs.add_widget(incs_radio)
+    layout.add_widget(incs)
+
+    e4g = SettingItem(panel = panel, title = "Crespo4G",disabled=False, desc = "")
+    e4g_radio = CheckBox(group='device',active=False)
+    e4g.add_widget(e4g_radio)
+    layout.add_widget(e4g)
+       
+    e3d = SettingItem(panel = panel, title = "d2",disabled=False, desc = "(At&t)")
+    e3d_radio = CheckBox(group='device',active=False)
+    e3d.add_widget(e3d_radio)
+    layout.add_widget(e3d)
+    
+    sen = SettingItem(panel = panel, title = "d2",disabled=False, desc = "(Sprint)")
+    sen_radio = CheckBox(group='device',active=False)
+    sen.add_widget(sen_radio)
+    layout.add_widget(sen)
+    
+    tb = SettingItem(panel = panel, title = "d2",disabled=False, desc = "(T-Mobile)")
+    tb_radio = CheckBox(group='device',active=False)
+    tb.add_widget(tb_radio)
+    layout.add_widget(tb)
+    
+    ama = SettingItem(panel = panel, title = "epicmtd",disabled=False, desc = "")
+    ama_radio = CheckBox(group='device',active=False)
+    ama.add_widget(ama_radio)
+    layout.add_widget(ama)
+    
+    g2 = SettingItem(panel = panel, title = "e730",disabled=False, desc = "")
+    g2_radio = CheckBox(group='device',active=False)
+    g2.add_widget(g2_radio)
+    layout.add_widget(g2)
+    
+    i4g = SettingItem(panel = panel, title = "e739",disabled=False, desc = "")
+    i4g_radio = CheckBox(group='device',active=False)
+    i4g.add_widget(i4g_radio)
+    layout.add_widget(i4g)
+    
+    sen = SettingItem(panel = panel, title = "fasinatemtd",disabled=False, desc = "")
+    sen_radio = CheckBox(group='device',active=False)
+    sen.add_widget(sen_radio)
+    layout.add_widget(sen)
+    
+    tb = SettingItem(panel = panel, title = "galaxysbmtd",disabled=False, desc = "")
+    tb_radio = CheckBox(group='device',active=False)
+    tb.add_widget(tb_radio)
+    layout.add_widget(tb)
+    
+    ama = SettingItem(panel = panel, title = "galaxysmtd",disabled=False, desc = "")
+    ama_radio = CheckBox(group='device',active=False)
+    ama.add_widget(ama_radio)
+    layout.add_widget(ama)
+    
+    g2 = SettingItem(panel = panel, title = "galaxys2",disabled=False, desc = "")
+    g2_radio = CheckBox(group='device',active=False)
+    g2.add_widget(g2_radio)
+    layout.add_widget(g2)
+    
+    i4g = SettingItem(panel = panel, title = "haida",disabled=False, desc = "")
+    i4g_radio = CheckBox(group='device',active=False)
+    i4g.add_widget(i4g_radio)
+    layout.add_widget(i4g)
+    
+    sen = SettingItem(panel = panel, title = "hallon",disabled=False, desc = "")
+    sen_radio = CheckBox(group='device',active=False)
+    sen.add_widget(sen_radio)
+    layout.add_widget(sen)
+    
+    tb = SettingItem(panel = panel, title = "hercules",disabled=False, desc = "")
+    tb_radio = CheckBox(group='device',active=False)
+    tb.add_widget(tb_radio)
+    layout.add_widget(tb)
+    
+    ama = SettingItem(panel = panel, title = "i777",disabled=False, desc = "")
+    ama_radio = CheckBox(group='device',active=False)
+    ama.add_widget(ama_radio)
+    layout.add_widget(ama)
+    
+    g2 = SettingItem(panel = panel, title = "i9100g",disabled=False, desc = "")
+    g2_radio = CheckBox(group='device',active=False)
+    g2.add_widget(g2_radio)
+    layout.add_widget(g2)
+    
+    i4g = SettingItem(panel = panel, title = "i9300",disabled=False, desc = "")
+    i4g_radio = CheckBox(group='device',active=False)
+    i4g.add_widget(i4g_radio)
+    layout.add_widget(i4g)
+
+    sen = SettingItem(panel = panel, title = "iyokan",disabled=False, desc = "")
+    sen_radio = CheckBox(group='device',active=False)
+    sen.add_widget(sen_radio)
+    layout.add_widget(sen)
+    
+    tb = SettingItem(panel = panel, title = "mango",disabled=False, desc = "")
+    tb_radio = CheckBox(group='device',active=False)
+    tb.add_widget(tb_radio)
+    layout.add_widget(tb)
+    
+    ama = SettingItem(panel = panel, title = "maguro",disabled=False, desc = "")
+    ama_radio = CheckBox(group='device',active=False)
+    ama.add_widget(ama_radio)
+    layout.add_widget(ama)
+    
+    g2 = SettingItem(panel = panel, title = "n7000",disabled=False, desc = "")
+    g2_radio = CheckBox(group='device',active=False)
+    g2.add_widget(g2_radio)
+    layout.add_widget(g2)
+    
+    i4g = SettingItem(panel = panel, title = "p1",disabled=False, desc = "")
+    i4g_radio = CheckBox(group='device',active=False)
+    i4g.add_widget(i4g_radio)
+    layout.add_widget(i4g)  
+    
+    ama = SettingItem(panel = panel, title = "p1c",disabled=False, desc = "")
+    ama_radio = CheckBox(group='device',active=False)
+    ama.add_widget(ama_radio)
+    layout.add_widget(ama)
+    
+    g2 = SettingItem(panel = panel, title = "p3",disabled=False, desc = "")
+    g2_radio = CheckBox(group='device',active=False)
+    g2.add_widget(g2_radio)
+    layout.add_widget(g2)
+    
+    i4g = SettingItem(panel = panel, title = "p3100",disabled=False, desc = "")
+    i4g_radio = CheckBox(group='device',active=False)
+    i4g.add_widget(i4g_radio)
+    layout.add_widget(i4g)
+    
+    sen = SettingItem(panel = panel, title = "p3110",disabled=False, desc = "")
+    sen_radio = CheckBox(group='device',active=False)
+    sen.add_widget(sen_radio)
+    layout.add_widget(sen)
+    
+    tb = SettingItem(panel = panel, title = "p3113",disabled=False, desc = "")
+    tb_radio = CheckBox(group='device',active=False)
+    tb.add_widget(tb_radio)
+    layout.add_widget(tb)
+    
+    ama = SettingItem(panel = panel, title = "p4",disabled=False, desc = "")
+    ama_radio = CheckBox(group='device',active=False)
+    ama.add_widget(ama_radio)
+    layout.add_widget(ama)
+    
+    g2 = SettingItem(panel = panel, title = "p4",disabled=False, desc = "(T-Mobile)")
+    g2_radio = CheckBox(group='device',active=False)
+    g2.add_widget(g2_radio)
+    layout.add_widget(g2)
+    
+    i4g = SettingItem(panel = panel, title = "p4",disabled=False, desc = "(Verizon)")
+    i4g_radio = CheckBox(group='device',active=False)
+    i4g.add_widget(i4g_radio)
+    layout.add_widget(i4g)
+
+    sen = SettingItem(panel = panel, title = "p4",disabled=False, desc = "(Wifi)")
+    sen_radio = CheckBox(group='device',active=False)
+    sen.add_widget(sen_radio)
+    layout.add_widget(sen)
+    
+    tb = SettingItem(panel = panel, title = "p5",disabled=False, desc = "")
+    tb_radio = CheckBox(group='device',active=False)
+    tb.add_widget(tb_radio)
+    layout.add_widget(tb)
+    
+    ama = SettingItem(panel = panel, title = "p5",disabled=False, desc = "(Wifi)")
+    ama_radio = CheckBox(group='device',active=False)
+    ama.add_widget(ama_radio)
+    layout.add_widget(ama)
+    
+    g2 = SettingItem(panel = panel, title = "p5100",disabled=False, desc = "")
+    g2_radio = CheckBox(group='device',active=False)
+    g2.add_widget(g2_radio)
+    layout.add_widget(g2)
+    
+    i4g = SettingItem(panel = panel, title = "p930",disabled=False, desc = "")
+    i4g_radio = CheckBox(group='device',active=False)
+    i4g.add_widget(i4g_radio)
+    layout.add_widget(i4g)  
+    
+    popup = Popup(background='atlas://images/eds/pop', title='Device Selection', content=main, auto_dismiss=True, size_hint=(None, None), size=(630, 500))
+    cancel.bind(on_release=popup.dismiss)
+    popup.open()
+ 
     
 def kernel_menu(self):
     try:
@@ -467,9 +715,26 @@ def source_menu(self):
             for x in p:
                 package_count += 1
     
-        aosp_source = CustomButton(text='Aosp ', pos_hint={'x':.0, 'y':.450}, size_hint=(.90, .06))
-        cm_source = CustomButton(text='Cyanogenmod', pos_hint={'x':.0, 'y':.350}, size_hint=(.90, .06))
-        clean = Button(text='Clean Out Old Rom Source', pos_hint={'x':.0, 'y':-.05}, size_hint=(.90, .06), background_color=(1.4, 0, 0, 0.6))
+            
+        branch = CustomButton(text='Select Branch', pos_hint={'x':.5, 'y':.450}, size_hint=(.40, .06))
+        device = CustomButton(text='Select Device', pos_hint={'x':.0, 'y':.450}, size_hint=(.40, .06))
+        jobs = BoxLayout(orientation='vertical', spacing=5, size_hint=(0.9, .20), pos_hint={'x':.09, 'y':-.07})
+        
+        stitle = Label(text='How Many [b]Sync[/b] Jobs, Default = 4', markup=True)
+        sslide = Slider(min=0, max=16, value=4)
+        s_value = Label(text='4', pos_hint={'x':-.45, 'y':-.445})
+        
+        mtitle = Label(text='How Many [b]Make[/b] Jobs, Default = 8', markup=True)
+        mslide = Slider(min=-0, max=8, value=8)
+        m_value = Label(text='8', pos_hint={'x':-.45, 'y':-.545})
+    
+        jobs.add_widget(stitle)
+        self.panel_layout.add_widget(s_value)
+        jobs.add_widget(sslide)
+        jobs.add_widget(mtitle)
+        self.panel_layout.add_widget(m_value)
+        jobs.add_widget(mslide)
+        clean = Button(text='Clean Out Old Rom Source', pos_hint={'x':.0, 'y':-.15}, size_hint=(.50, .06), background_color=(1.4, 0, 0, 0.6))
     
         if package_count == 0:
             pass
@@ -480,17 +745,18 @@ def source_menu(self):
             pass
         else:
             self.panel_layout.add_widget(i_packages)
-        self.panel_layout.add_widget(aosp_source)
-        self.panel_layout.add_widget(cm_source)
+        self.panel_layout.add_widget(branch)
+        self.panel_layout.add_widget(device)
+        self.panel_layout.add_widget(jobs)
         self.panel_layout.add_widget(clean)
-
-        def aosp_base(instance):
-            aosp_select(self)
-        aosp_source.bind(on_release=aosp_base) 
+    
+        def show_branch(instance):
+            branch_select(self)
+        branch.bind(on_release=show_branch) 
         
-        def cm_base(instance):
-            cm_select(self)
-        cm_source.bind(on_release=cm_base)        
+        def show_device(instance):
+            device_select(self)
+        device.bind(on_release=show_device)        
     
         def clean_files(instance):
             root = BoxLayout(orientation='vertical', spacing=20)
@@ -530,5 +796,5 @@ def source_menu(self):
             pass
         else:
             i_packages.bind(on_release=install_packages)
-    except: 
+    except:
         no_os(self)
